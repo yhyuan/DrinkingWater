9e9b157891b4fc9d039b71811803b6f6dc57e1e0
Jerry Yuan <yhyuan@gmail.com>
2014-08-15 09:24:01 -0400

    initial commit

---
+++ DrinkingWater.py
@@ -0,0 +1,47 @@
+import sys
+reload(sys)
+sys.setdefaultencoding("latin-1")
+
+import xlrd, arcpy, string, os, zipfile, fileinput, time
+from datetime import date
+start_time = time.time()
+
+INPUT_PATH = "input"
+OUTPUT_PATH = "output"
+if arcpy.Exists(OUTPUT_PATH + "\\DrinkingWater.gdb"):
+	os.system("rmdir " + OUTPUT_PATH + "\\DrinkingWater.gdb /s /q")
+os.system("del " + OUTPUT_PATH + "\\*DrinkingWater*.*")
+arcpy.CreateFileGDB_management(OUTPUT_PATH, "DrinkingWater", "9.3")
+arcpy.env.workspace = OUTPUT_PATH + "\\DrinkingWater.gdb"
+
+def createFeatureClass(featureName, featureData, featureFieldList, featureInsertCursorFields):
+	print "Create " + featureName + " feature class"
+	featureNameNAD83 = featureName + "_NAD83"
+	featureNameNAD83Path = arcpy.env.workspace + "\\"  + featureNameNAD83
+	arcpy.CreateFeatureclass_management(arcpy.env.workspace, featureNameNAD83, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
+	# Process: Define Projection
+	arcpy.DefineProjection_management(featureNameNAD83Path, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+	# Process: Add Fields	
+	for featrueField in featureFieldList:
+		arcpy.AddField_management(featureNameNAD83Path, featrueField[0], featrueField[1], featrueField[2], featrueField[3], featrueField[4], featrueField[5], featrueField[6], featrueField[7], featrueField[8])
+	# Process: Append the records
+	cntr = 1
+	try:
+		with arcpy.da.InsertCursor(featureNameNAD83, featureInsertCursorFields) as cur:
+			for rowValue in featureData:
+				cur.insertRow(rowValue)
+				cntr = cntr + 1
+	except Exception as e:
+		print "\tError: " + featureName + ": " + e.message
+	# Change the projection to web mercator
+	arcpy.Project_management(featureNameNAD83Path, arcpy.env.workspace + "\\" + featureName, "PROJCS['WGS_1984_Web_Mercator_Auxiliary_Sphere',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Mercator_Auxiliary_Sphere'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],PARAMETER['Standard_Parallel_1',0.0],PARAMETER['Auxiliary_Sphere_Type',0.0],UNIT['Meter',1.0]]", "NAD_1983_To_WGS_1984_5", "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+	arcpy.Delete_management(featureNameNAD83Path, "FeatureClass")
+	print "Finish " + featureName + " feature class."
+wb = xlrd.open_workbook('input\\Data\\DWS_Geo-Spatial_Coordinates_prototype_map_aug142014.xlsx')
+sh = wb.sheet_by_name(u'page')
+for rownum in range(1, sh.nrows):
+	row = sh.row_values(rownum)
+	print row
+
+elapsed_time = time.time() - start_time
+print elapsed_time
